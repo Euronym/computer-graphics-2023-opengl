f408bca2692bd07cc928b701dbca4d96012a4052
Euronym <brunos-martins@hotmail.com>
2023-04-08 18:40:39 -0300

    change run

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #/bin/bash
 
-g++ main.cpp -o main -lGL -lGLU -lglut -lm
-#g++ stickman_walk.cpp -o main -lGL -lGLU -lglut -lm
+g++ main.cpp -I "./irrKlang-64bit-1.6.0/include" -L "/usr/lib" ./irrKlang-64bit-1.6.0/bin/linux-gcc-64/libIrrKlang.so -I include -o main -lGL -lGLU -lglut -lm -pthread
